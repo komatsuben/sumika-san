39c459b88f88f96a3a2c65620bc0fe70c655e200
Benedict Halim <benedicthalim1@gmail.com>
2024-12-26 13:34:33 +0700

    vercel setup

--- sumika/sumika/settings.py
+++ sumika/sumika/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)8^2%a#s*8#)nc!&*#c-^7_i8^*jaxt#er2+8l(zahd6l0e8^*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
